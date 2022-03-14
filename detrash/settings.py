c59b0c8009fc77123014ea42385f048b24f3a6f3
Daniel Griffin <dgriffin@sealworks.com>
2022-03-14 09:22:57 -0400

    new secret key, store as env variable

--- detrash/settings.py
+++ detrash/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=*!ib=#x_vg72-p36(g*k+hs$pa$*+83zn*#tkq386i2p*k539'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
